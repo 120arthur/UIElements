e900c65e90f5ac9e7302167049d2311550d3c9cb
arthurfernndolucas <arthurlucasfernando@gmail.com>
2020-12-21 14:55:20 -0300

    making a window, Buttons and callBack

---
+++ Assets/QuickTool/Editor/QuickTool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class QuickTool : EditorWindow
+{
+    [MenuItem("QuickTool/Open _%#T")]
+    public static void ShowWindow()
+    {
+        var window = GetWindow<QuickTool>();
+        window.titleContent = new GUIContent("QuickTool");
+        window.minSize = new Vector2(250, 50);
+    }
+
+}
